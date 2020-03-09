331491d36076cfd53707c34936d297999f15bf7f
deficts <diegoar7@live.com>
2020-03-08 21:00:15 -0600

    agregando clases de arista y estado para el AFN

---
+++ src/com/deficts/Arista.java
@@ -0,0 +1,25 @@
+package com.deficts;
+
+public class Arista {
+    private char llave;
+    private Estado inicio;
+    private Estado fin;
+
+    public Arista(char llave, Estado inicio, Estado fin){
+        this.llave = llave;
+        this.inicio = inicio;
+        this.fin = fin;
+    }
+
+    public char getLlave(){
+        return llave;
+    }
+
+    public Estado getFin() {
+        return fin;
+    }
+
+    public Estado getInicio() {
+        return inicio;
+    }
+}
---
+++ src/com/deficts/Estado.java
@@ -0,0 +1,64 @@
+package com.deficts;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Estado {
+    private boolean esFinal = false;
+
+    private List<Arista> salidas = new ArrayList<Arista>();
+    private List<Arista> entradas = new ArrayList<Arista>();
+
+    public void setFinal(boolean esFinal){
+        this.esFinal= esFinal;
+    }
+
+    public boolean esFinal(){
+        return this.esFinal;
+    }
+
+    public List<Arista> getSalidas() {
+        return salidas;
+    }
+    public void addSalida(Arista a){
+        this.salidas.add(a);
+    }
+
+    public List<Arista> getEntradas() {
+        return entradas;
+    }
+    public void addEntrada(Arista a){
+        this.entradas.add(a);
+    }
+
+    public boolean containsSalida(char x){
+        for(Arista a : salidas){
+            if (a.getLlave() == x){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Estado salto(char x){
+        Estado nuevo = new Estado();
+
+        for (Arista a : salidas){
+            if (a.getLlave() == x){
+                return a.getFin();
+            }
+        }
+        return nuevo;
+    }
+
+    public void removeSalida(char x){
+        Iterator<Arista> it = salidas.iterator();
+        while(it.hasNext()){
+            Arista a = it.next();
+            if(a.getLlave()==x){
+                it.remove();
+            }
+        }
+    }
+}
